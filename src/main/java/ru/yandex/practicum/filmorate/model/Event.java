965210dd8bd3a898661cf247e4c85efcb76162ed
Julia Kozlova <kozjuliajulia@yandex.ru>
2023-06-24 14:38:52 +0300

    fit: проверка переменных и параметров перенесена из контроллеров в сервисы

--- src/main/java/ru/yandex/practicum/filmorate/model/Event.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Event.java
@@ -1,22 +1,22 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NonNull;
 
 @Data
-@AllArgsConstructor
 @Builder
 public class Event {
 
     @NonNull
-    private long eventId;
-    private long userId;
-    private long timestamp;
+    private Long eventId;
+    @NonNull
+    private Long userId;
+    @NonNull
+    private Long timestamp;
     private String eventType; // одно из значений LIKE, REVIEW или FRIEND
     private String operation; // одно из значениий REMOVE, ADD, UPDATE
     @NonNull
-    private long entityId;  // идентификатор сущности, с которой произошло событие
+    private Long entityId;  // идентификатор сущности, с которой произошло событие
 
 }
