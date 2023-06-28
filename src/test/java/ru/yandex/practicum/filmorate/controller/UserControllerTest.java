e21fa0f73821196e5a8f25e37807ca0aeff84bcf
Julia Kozlova <kozjuliajulia@yandex.ru>
2023-06-28 12:30:37 +0300

    fit: checkstyle

--- src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
@@ -4,7 +4,6 @@ import ru.yandex.practicum.filmorate.exception.UserNotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.UserService;
-import ru.yandex.practicum.filmorate.storage.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.FriendStorage;
 import ru.yandex.practicum.filmorate.storage.GradeStorage;
 import ru.yandex.practicum.filmorate.storage.LikeStorage;
